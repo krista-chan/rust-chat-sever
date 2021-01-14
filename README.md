96ab5f4b4e09f21c2edcd23ed7a329aaf2560d78
loggers-bro <qbotdedv84@gmail.com>
2021-01-14 08:59:46 +0000

    Big progress, almost added messages

---
+++ README.md
@@ -0,0 +1,18 @@
+# Cutie chat server API documentation
+
+## Note: this is very WIP and will have bad error catching
+
+---
+
+## gateway
+
+The chat gateway in its current state is very simple, with no login function and only a way to register a user
+
+|Opcode|Data response|
+|------|-------------|
+|Connect|Returns json with the new user's token and user ID|
+|More coming soon|...|
+
+## Getting the client
+
+I'm developing one dont worry
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,70 @@
-extern crate tungstenite;
-extern crate tokio;
 extern crate serde_json;
+extern crate tokio;
+extern crate tungstenite;
+extern crate uuid;
 
 use serde::{Deserialize, Serialize};
-use std::net::TcpListener;
-use tungstenite::{Message, server::accept, protocol::{frame::coding, CloseFrame}};
 use serde_json::{from_str, Value};
+use std::net::TcpListener;
+use std::sync::{Arc, RwLock};
+use tungstenite::{
+    protocol::{frame::coding, CloseFrame},
+    server::accept,
+    Message,
+};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
-    let users: std::collections::HashMap<&str, User> = std::collections::HashMap::new();
-    let server = TcpListener::bind("127.0.0.1:9001").expect("Unable to start server");
-    for connection in server.incoming() {
-        tokio::spawn(async move {
-            let mut ws = accept(connection.unwrap()).unwrap();
-            ws.write_message("You connected".into()).unwrap();
-            loop {
-                let msg = ws.read_message().unwrap();
-                let msg_json: WSMessage = from_str(&msg.to_text().unwrap()).unwrap();
-                let close_opts: CloseFrame = CloseFrame {code: coding::CloseCode::Normal, reason: std::borrow::Cow::default()};
-                match msg_json.opcode {
-                    Opcodes::Connect => {
-                        if msg_json.data["login_pkg"] == Value::Null {
-                            ws.write_message(Message::Binary("{\"error\":\"missing login data\"}".into())).unwrap();
-                            ws.close(Option::from(close_opts)).unwrap();
+    let users: Arc<RwLock<std::collections::HashMap<String, User>>> =
+        Arc::new(RwLock::new(std::collections::HashMap::new()));                                    // declare a structure to hold the connections as they connect and disconnect from the server
+    let _channels: std::collections::HashMap<String, Channel> = std::collections::HashMap::new();   // declare a structure to hold text channels
+    let server = TcpListener::bind("127.0.0.1:9001").expect("Unable to start server");              // start the ws server (BLOCKING)
+    for connection in server.incoming() {                                                           // as a connection is recieved ...
+        let users = users.clone();                                                                  // ... clone users to be able to write a new user to the hashmap ...
+        tokio::spawn(async move {                                                                   // ... and then spawn a new async thread with tokio
+            let mut ws = accept(connection.unwrap()).unwrap();                                      // accept the ws connection (this will be our rw socket)
+            ws.write_message("{\"message\":\"You connected\"}".into())                              // send a message on connect (this will arrive as a buffer)
+                .unwrap();
+            loop {                                                                                  // start a blocking loop ...
+                let users = users.clone();                                                          // ... where users is cloned again
+                let msg = ws.read_message().unwrap();                                               // since this is triggered every time a new connection occurs each thread will have its own socket
+                let msg_json: WSMessage = from_str(&msg.to_text().unwrap()).unwrap();               // use serde_json to parse the incoming message to a weak typed json-like structure
+                let close_opts: CloseFrame = CloseFrame {                                           // instantiate a CloseFrame incase we need to close the ws connection
+                    code: coding::CloseCode::Normal,
+                    reason: std::borrow::Cow::default(),
+                };
+                match msg_json.t {                                                                  // here we handle message opcodes
+                    Opcodes::Connect => {                                                           // opcode Connect will check for lack of data ...
+                        if msg_json.d == Value::Null {
+                            ws.write_message(Message::Binary(
+                                "{\"error\":\"missing login data\"}".into(),                        // ... and throw an error to the user ...
+                            ))
+                            .unwrap();
+                            ws.close(Option::from(close_opts)).unwrap();                            // ... after which it will close the ws and terminate the thread
                         } else {
-                            println!("{}", msg_json.data["login_pkg"]);
+                            if msg_json.d["login_pkg"] == Value::Null {                             // here, if there's no data in the login_pkg field then generate two uuids for the token and the user ID
+                                let token = gen_uuid();
+                                let id = gen_uuid();
+                                users.write().unwrap().insert(
+                                    id.clone(),
+                                    User::new(
+                                        String::from("Unnamed user"),
+                                        token.clone(),
+                                        id.clone(),
+                                    ),
+                                );
+                                ws.write_message(Message::Binary(
+                                    format!(
+                                        "{{\"connection\": {{\"token\":\"{}\", \"id\":\"{}\"}}}}",  // now we can send the newly generated login data to the user so that they can use it for later and whatnot
+                                        token.to_owned(),
+                                        id.to_owned()
+                                    )
+                                    .into(),
+                                ))
+                                .unwrap();
+                                println!("{:?}", users.read().unwrap());                            // as a dev thing, log the users hashmap
+                            }
                         }
                     }
                 }
@@ -34,17 +73,38 @@ async fn main() {
     }
 }
 
+pub fn gen_uuid() -> String {
+    let uuid = Uuid::new_v4();
+    uuid.to_string()
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum Opcodes {
     Connect,
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct WSMessage {
-    opcode: Opcodes,
-    data: serde_json::Value
+    t: Opcodes,
+    d: serde_json::Value,
 }
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct User {
+    id: String,
+    name: String,
+    token: String,
+}
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Channel {}
+
+impl User {
+    pub fn new(name: String, token: String, id: String) -> Self {
+        User {
+            name: name,
+            token: token,
+            id: id,
+        }
+    }
 }
